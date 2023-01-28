953f91a57d0c6031c79fbecaba30e38afd8bcebc
tretornesp <xabier.iglesias.perez@udc.es>
2023-01-28 01:11:16 +0100

    Error control on image upload

--- server/app/service/uploads_service.py
+++ server/app/service/uploads_service.py
@@ -12,18 +12,21 @@ class UploadsService:
     @staticmethod
     def upload_photo(file, user_id):
         repository = MongoRepository()
-        
-        image = Image.open(file)
-        image.thumbnail((300, 300), Image.ANTIALIAS)
-        RouteMaster.log("Image size: " + str(image.size))
-        buffered = BytesIO()
-        image.save(buffered, format="JPEG")
+        try:
+            image = Image.open(file)
+            image.thumbnail((300, 300), Image.ANTIALIAS)
+            RouteMaster.log("Image size: " + str(image.size))
+            buffered = BytesIO()
+            image.save(buffered, format="JPEG")
 
-        photo = {
-            "expires": int(time.time()) + 3600,
-            "user_id": str(user_id),
-            "uses": 0,
-            "data": base64.b64encode(buffered.getvalue()).decode('utf-8')
-        }
+            photo = {
+                "expires": int(time.time()) + 3600,
+                "user_id": str(user_id),
+                "uses": 0,
+                "data": base64.b64encode(buffered.getvalue()).decode('utf-8')
+            }
+        except Exception as e:
+            RouteMaster.log("Error uploading photo: " + str(e))
+            return None
 
         return str(repository.upload_photo(photo))
